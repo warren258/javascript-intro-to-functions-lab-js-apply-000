1184090e147c4110457b5b861bf4eba7f9096a47
Warren McDonald <warrenjmac@mail.com>
2019-07-07 08:16:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   return "I can\'t hear you!";
   }else if  (string === string.toUpperCase()){
     return "YES INDEED!";
-  }else  (string === iLoveYouGrandma){
+  }else  if (string === iLoveYouGrandma){
     return "I love you, too";
   }
 }
